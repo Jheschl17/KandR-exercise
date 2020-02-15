8d3e8bae4ecebb5046bcafc3d0fbcd2cba396126
jonas.heschl@gmx.at <jonas.heschl@gmx.at>
2020-02-15 17:27:36 +0100

    task 3-3: expand

--- expand.c
+++ expand.c
@@ -7,25 +7,42 @@ void expand_part(char from, char to, char out[]);
 
 int main()
 {
-
+    char s1[] = "----asdfa-z2314a--1-5-8a--";
+    char s2[100];
+    expand(s1, s2);
+    printf("%s\n", s2);
 }
 
 void expand(char s1[], char s2[])
 {
     int j = 0;
     for (int i = 0; i < strlen(s1); i++, j++) {
-        if (isupper(s1[i]))
-            ;
-        else if (islower(s1[i]))
-            ;
-        else if (isdigit(s1[i]))
-            ;
-        else
+        if (
+            s1[i] == '-'
+            &&
+            (
+                (islower(s1[i - 1]) && islower(s1[i + 1]))
+                ||
+                (isupper(s1[i - 1]) && isupper(s1[i + 1]))
+                ||
+                (isdigit(s1[i - 1]) && isdigit(s1[i + 1]))
+            )
+        ) {
+            char part[30];
+            expand_part(s1[i - 1] + 1, s1[i + 1], part);
+            j += (int) strlen(part) - 1;
+            i += 1;
+            strcat(s2, part);
+        } else
             s2[j] = s1[i];
     }
+    s2[j] = '\0';
 }
 
-void expand_part(char part[], char out[])
+void expand_part(char from, char to, char out[])
 {
-
+    int i;
+    for (i = from; i <= to; ++i)
+        out[i - from] = (char) i;
+    out[i - from] = '\0';
 }
